b1fe5dcb29a48a648439d81e4e47e1fc4cc235eb
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2021-07-09 17:48:37 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  // The size of our starting canvas: 400px width, 400px height
+  createCanvas(400, 400);
+}
+
+function draw() {
+  // The same as doing: background(220); or --> background(220,220,220);
+  background("gray");
+}
